b4e840f35b862f2d085d803412addaabaaa9169b
Ilonah-Jean <decolongonilonah@isufst.edu.ph>
2024-10-30 12:06:30 +0800

    Add files via upload

---
+++ my_first_website/members/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse # type: ignore
+from django.template import loader # type: ignore
+from .models import Member
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'member' : mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
